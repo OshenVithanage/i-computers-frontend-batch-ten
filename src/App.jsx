c37a980005ec9688b0ed265010e3284807a44766
Oshen Vithanage <lagmomy@gmail.com>
2026-03-09 17:19:01 +0530

    Day14✏️

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import ProductCard from './components/productCard'
 import TrendingProducts from './components/trendingProducts'
+import HomePage from './pages/homePage'
+import AdminPage from './pages/adminPage'
 
 function App() {
   return (
-    <>
-      <div>
-        <TrendingProducts />
-      </div>
-    </>
+    <div className='w-full h-screen flex justify-center items-center relative bg-primary text-secondary'>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/admin" element={<AdminPage />} />
+      </Routes>
+    </div>
   )
 }
 
---
+++ src/pages/adminPage.jsx
@@ -0,0 +1,12 @@
+export default function AdminPage() {
+    return (
+        <div className="w-full h-screen flex items-center">
+            <div className="w-[300px] h-full bg-accent ">
+
+            </div>
+            <div className="w-[1200px] h-full bg-red-900">
+
+            </div>
+        </div>
+    )
+}
